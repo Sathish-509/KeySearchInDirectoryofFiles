7add1411137d6e8cb0db2e3f9ea9d536b64adce6
Sathish <sathish9yerramsetti@gmail.com>
2020-01-05 02:39:01 +0800

    async await version completed

---
+++ app_asyncawait.mjs
@@ -0,0 +1,38 @@
+import { join } from 'path';
+import {default as fsWithCallbacks} from 'fs'
+const fs = fsWithCallbacks.promises
+
+const directoryPath = process.argv[2]; 
+const searchKey = process.argv[3];
+
+async function rreaddir (dir, allFiles = []) {
+  const files = (await fs.readdir(dir)).map(f => join(dir, f))
+  allFiles.push(...files)
+  await Promise.all(files.map(async f => (
+    (await fs.stat(f)).isDirectory() && rreaddir(f, allFiles)
+  )))
+  return allFiles;
+}
+
+async function checkStringInFile(file) {
+    const data = await fs.readFile(file, {encoding: 'utf8'});
+    return data;
+}
+
+async function fetchData() {
+  const list = await rreaddir(directoryPath);
+  const finalList = [];
+  for (let index in list) {
+    try {
+      const data = await checkStringInFile(list[index]);
+      if(data.includes(searchKey) > 0) {
+          finalList.push(list[index]);
+      }
+    } catch(err) {
+      // console.log(err);
+    }
+  }
+  console.log(finalList);
+}
+
+fetchData();
